0622d1d91134f2c389a24eee7f66ef532f1cfdb6
Matthew Serretta <matthew.serretta@gmail.com>
2020-02-11 14:12:58 +1100

    Main now draws a grid

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Dimension;
 
-public class Main {
 
+public class Main extends JFrame implements Runnable{
+	
+	private class Canvas extends JPanel{
+		public Canvas() {
+			setPreferredSize(new Dimension(1280, 720));
+		}
+		
+		@Override
+		public void paint(Graphics g) {
+			for(int x=10; x<700; x = x + 35) {
+				for (int y=10; y<700; y = y + 35) {
+					g.drawRect(x, y, 35, 35);					
+				}
+			}
+		}
+	}
+	
     public static void main(String[] args) {
-	System.out.println("~~~~~");
-	System.out.println("Sheep and Wolves");
-	System.out.println("~~~~~");
+    	Main window = new Main();
+    	window.run();
     }
+    
+    private Main() {
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	this.setContentPane(new Canvas());
+    	this.pack();
+    	this.setVisible(true);
+    }
+    
+    @Override
+    public void run() {
+    	this.repaint();
+    }
+    
+    
 }
