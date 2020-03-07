9714b5ec1593bf85ddc60b625f46883837db5337
Matthew Serretta <matthew.serretta@gmail.com>
2020-03-07 13:18:23 +1100

    Added a thread.sleep to set a frame rate of ~50f/s and reduce unnecessary CPU cycle wastage

--- src/Stage.java
+++ src/Stage.java
@@ -8,35 +8,43 @@ public class Stage {
 	private Character sheep;
 	private Character shepherd;
 	private Character wolf;
-	
+
 	private List<bos.RelativeMove> moves;
-	
+
 	private Instant timeOfLastMove;
-	
 
 	public Stage() {
 		grid = new Grid();
 		sheep = new Sheep(grid.getRandomCell());
 		shepherd = new Shepherd(grid.getRandomCell());
 		wolf = new Wolf(grid.getRandomCell());
-		
+
 		moves = new ArrayList<bos.RelativeMove>();
 		moves.add(new bos.MoveUp(grid, sheep));
-		
+		moves.add(new bos.MoveUp(grid, sheep));
+		moves.add(new bos.MoveUp(grid, sheep));
+
 		timeOfLastMove = Instant.now();
 	}
+
 	public void update() {
-		if(moves.size() > 0 && timeOfLastMove.plus(Duration.ofSeconds(2)).isBefore(Instant.now())) {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		if (moves.size() > 0 && timeOfLastMove.plus(Duration.ofSeconds(2)).isBefore(Instant.now())) {
 			timeOfLastMove = Instant.now();
 			moves.remove(0).perform();
-			
-		} else if(moves.size() == 0 && timeOfLastMove.plus(Duration.ofSeconds(20)).isBefore(Instant.now())) {
+
+		} else if (moves.size() == 0 && timeOfLastMove.plus(Duration.ofSeconds(20)).isBefore(Instant.now())) {
 			System.exit(0);
 		}
 	}
-	
+
 	public void paint(Graphics g, Point mouseLocation) {
-		grid.paint(g,  mouseLocation);
+		grid.paint(g, mouseLocation);
 		sheep.paint(g);
 		shepherd.paint(g);
 		wolf.paint(g);
